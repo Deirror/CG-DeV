22eddb9048178f729f5c19277d59e54d572634c7
Deirror <alexmailbg@gmail.com>
2025-05-30 19:52:34 +0300

    Add code with above one triangle - video "OpenGL Element Array Buffers"

--- JKingGL/OpenGL/src/Engine.cpp
+++ JKingGL/OpenGL/src/Engine.cpp
@@ -28,21 +28,24 @@ void Engine::run(GLFWwindow* window)
 	{
 		0.0f, 0.0f,
 		960.0f, 0.0f,
-		0.0f, 540.0f
+		480.0f, 270.0f,
+		0.0f, 540.0f,
+		960.0f, 540.0f
 	};
 
 	unsigned int indices[] =
 	{
-		0, 1, 2
+		0, 1, 2,
+		2, 3, 4
 	};
 
 	glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-	glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(10, 0, 0));
-	glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(10, 0, 0));
+	glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
+	glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
 
 	glm::mat4 mvp = proj * view * model;
 
-	VertexBuffer vb(verts, 2 * sizeof(float) * 3);
+	VertexBuffer vb(verts, 2 * sizeof(float) * 5);
 
 	VertexBufferLayout layout;
 	layout.Push<float>(2);
@@ -50,7 +53,7 @@ void Engine::run(GLFWwindow* window)
 	VertexArray va;
 	va.AddVertexBuffer(vb, layout);
 
-	IndexBuffer ib(indices, 3);
+	IndexBuffer ib(indices, 6);
 
 	Shader shader("res/shaders/Basic.shader");
 	shader.SetUniform4f("u_Color", 1.0f, 0.0f, 1.0f, 1.0f);
