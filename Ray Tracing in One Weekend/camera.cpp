06822a156f63871e35d3a9489b76e34bfedcda35
Deirror <alexmailbg@gmail.com>
2025-06-08 11:42:13 +0300

    Add antialiasing by sampling adjacent vertexes

---
+++ Ray Tracing in One Weekend/camera.cpp
@@ -0,0 +1,82 @@
+#include "camera.h"
+
+#include <iostream>
+#include <cstdlib>
+
+inline double random_double() {
+    return std::rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    return min + (max-min)*random_double();
+}
+
+void camera::initialize() {
+    image_height = int(image_width / aspect_ratio);
+    if (image_height < 1) image_height = 1;
+
+    pixel_samples_scale = 1.0 / samples_per_pixel;
+
+    center = point3(0, 0, 0);
+
+    double focal_length = 1.0;
+    double viewport_height = 2.0;
+    double viewport_width = viewport_height * (double(image_width) / image_height);
+
+    vec3 viewport_u = vec3(viewport_width, 0, 0);
+    vec3 viewport_v = vec3(0, -viewport_height, 0);
+
+    pixel_delta_u = viewport_u / image_width;
+    pixel_delta_v = viewport_v / image_height;
+
+    point3 viewport_upper_left = center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+    pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+}
+
+color camera::ray_color(const ray& r, const hittable& world) const {
+    hit_record rec;
+
+    if (world.hit(r, interval(0, infinity), rec)) {
+        return 0.5 * (rec.normal + color(1, 1, 1));
+    }
+
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+}
+
+void camera::render(const hittable& world) {
+    initialize();
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; j++) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+        for (int i = 0; i < image_width; i++) {
+            color pixel_color(0,0,0);
+            for (int sample = 0; sample < samples_per_pixel; sample++) {
+                ray r = get_ray(i, j);
+                pixel_color += ray_color(r, world);
+            }
+            write_color(std::cout, pixel_samples_scale * pixel_color);
+        }
+    }
+
+    std::clog << "\rDone.                 \n";
+}
+
+ray camera::get_ray(int i, int j) const {
+    auto offset = sample_square();
+    auto pixel_sample = pixel00_loc
+                        + ((i + offset.x()) * pixel_delta_u)
+                        + ((j + offset.y()) * pixel_delta_v);
+
+    auto ray_origin = center;
+    auto ray_direction = pixel_sample - ray_origin;
+
+    return ray(ray_origin, ray_direction);
+}
+
+vec3 camera::sample_square() const {
+    return vec3(random_double() - 0.5, random_double() - 0.5, 0);
+}
---
+++ Ray Tracing in One Weekend/camera.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "vec3.h"
+#include "ray.h"
+#include "color.h"
+#include "hittable.h"
+#include "interval.h"
+
+class camera {
+  public:
+    double aspect_ratio = 1.0;  
+    int    image_width  = 100;  
+    int    samples_per_pixel = 10;
+
+    void render(const hittable& world);
+
+  private:
+    int    image_height;   
+    double pixel_samples_scale;
+    point3 center;         
+    point3 pixel00_loc;    
+    vec3   pixel_delta_u;  
+    vec3   pixel_delta_v;  
+
+    void initialize();
+    color ray_color(const ray& r, const hittable& world) const;
+
+    ray get_ray(int i, int j) const;
+    vec3 sample_square() const;
+};
